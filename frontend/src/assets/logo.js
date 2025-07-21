ec2eff4a0933c15714d71b46799badaeb1fcea57
Ömer Faruk Günay <116501544+ofarukgunay@users.noreply.github.com>
2025-07-21 13:46:52 +0300

    Update

--- frontend/src/assets/logo.js
+++
@@ -1,7 +0,0 @@
-// Base64 logo için örnek kod
-// Fotoğrafınızı base64'e çevirin ve buraya yapıştırın
-
-const LOGO_BASE64 =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/5+hHgAHggJ/PchI7wAAAABJRU5ErkJggg==";
-
-export default LOGO_BASE64;
